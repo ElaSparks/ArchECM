94c93de123a3301c221203ebae62b07d40fb4079
ElaSparks <danilka232001@gmail.com>
2021-04-04 20:23:14 +0700

    Signals been added and full encapsulated.

---
+++ src/SignalSource/Signals.cpp
@@ -0,0 +1,41 @@
+#include "../../include/MySimpleComputer.h"
+
+MySimpleComputer* globalPC = nullptr;
+
+void MySimpleComputer::stopHandler(int signal)
+{
+    globalPC->newTimer.it_interval.tv_sec = 0;
+    globalPC->newTimer.it_interval.tv_usec = 0;
+    globalPC->newTimer.it_value.tv_sec = 0;
+    globalPC->newTimer.it_value.tv_usec = 0;
+    setitimer(ITIMER_REAL, &globalPC->newTimer, &globalPC->oldTimer);
+}
+void MySimpleComputer::runHandler(int signal)
+{
+    if (globalPC->instructionCounter != 99) {
+        globalPC->DrawAll();
+        ++globalPC->instructionCounter;
+    } else {
+        globalPC->DrawAll();
+        stopHandler(0);
+    }
+}
+void MySimpleComputer::oneStep()
+{
+    globalPC = this;
+    signal(SIGALRM, runHandler);
+    newTimer.it_interval.tv_sec = 0;
+    newTimer.it_interval.tv_usec = 0;
+    newTimer.it_value.tv_sec = 0;
+    newTimer.it_value.tv_usec = 1;
+    setitimer(ITIMER_REAL, &newTimer, &oldTimer);
+}
+void MySimpleComputer::runEachMemory()
+{
+    signal(SIGALRM, runHandler);
+    newTimer.it_interval.tv_sec = 1;
+    newTimer.it_interval.tv_usec = 0;
+    newTimer.it_value.tv_sec = 1;
+    newTimer.it_value.tv_usec = 0;
+    setitimer(ITIMER_REAL, &newTimer, &oldTimer);
+}
