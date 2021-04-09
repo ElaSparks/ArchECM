79be064b3f3e940395ea21a845aacf7ea88bf8b4
ElaSparks <danilka232001@gmail.com>
2021-04-09 21:46:16 +0700

    Hadling of reset button without run signal handler.

--- src/SignalSource/Signals.cpp
+++ src/SignalSource/Signals.cpp
@@ -4,13 +4,15 @@ MySimpleComputer* globalPC = nullptr;
 
 void MySimpleComputer::stopHandler(int signal)
 {
-    globalPC->newTimer.it_interval.tv_sec = 0;
-    globalPC->newTimer.it_interval.tv_usec = 0;
-    globalPC->newTimer.it_value.tv_sec = 0;
-    globalPC->newTimer.it_value.tv_usec = 0;
-    globalPC->sc_regSet(IGNORE_IMPULS, 1);
-    globalPC->rk_switchecho();
-    setitimer(ITIMER_REAL, &globalPC->newTimer, &globalPC->oldTimer);
+    if (globalPC) {
+        globalPC->newTimer.it_interval.tv_sec = 0;
+        globalPC->newTimer.it_interval.tv_usec = 0;
+        globalPC->newTimer.it_value.tv_sec = 0;
+        globalPC->newTimer.it_value.tv_usec = 0;
+        globalPC->sc_regSet(IGNORE_IMPULS, 1);
+        globalPC->rk_switchecho();
+        setitimer(ITIMER_REAL, &globalPC->newTimer, &globalPC->oldTimer);
+    }
 }
 void MySimpleComputer::runHandler(int signal)
 {
@@ -40,7 +42,7 @@ void MySimpleComputer::oneStep()
     setitimer(ITIMER_REAL, &newTimer, &oldTimer);
 }
 void MySimpleComputer::runEachMemory()
-{ //сделать Эхо чтобы во время проги ввседенные символы не сыпались в терминал
+{
     rk_switchecho();
     globalPC = this;
     sc_regSet(IGNORE_IMPULS, 0);
